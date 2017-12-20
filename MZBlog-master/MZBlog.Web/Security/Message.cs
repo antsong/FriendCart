0e641d0a9402c4406313ec5e244ff31472db195d
szy <szy@bsj>
2017-12-20 18:05:11 +0800

    commit

--- MZBlog-master/MZBlog.Web/Security/Message.cs
+++ MZBlog-master/MZBlog.Web/Security/Message.cs
@@ -27,19 +27,6 @@ namespace MZBlog.Web.Security
             return Encoding.UTF8.GetString(Instance.DownloadData(path));
         }
 
-        public static string SendSms(string url, string userId, string token)
-        {
-            var dic = new Dictionary<string, string>();
-            dic.Add("user_id", userId);
-            dic.Add("sign_no", "cy6MTcmP");
-            dic.Add("timestamp", DateTime.Now.ToString("yyyyMMddHHmmss"));
-            dic.Add("ext", "1");
-            dic.Add("mobiles", "['13871298164','13618625064']");
-            dic.Add("content", "['NO0003319','建总副总','建总总经理']");
-            dic.Add("type", "2");
-            return Send(url, dic, token);
-        }
-
         public static string SendSms(string url, string userId, string token, string[] mobiles, string content)
         {
             var dic = new Dictionary<string, string>();
@@ -50,20 +37,6 @@ namespace MZBlog.Web.Security
             dic.Add("mobiles", string.Format("['{0}']", string.Join("','", mobiles)));
             dic.Add("content", content);
             dic.Add("type", "2");
-            return Send(url, dic, token);
-        }
-
-        public static string SendTemplateSms(string url, string userId, string token)
-        {
-            var dic = new Dictionary<string, string>();
-            dic.Add("user_id", userId);
-            dic.Add("sign_no", "cy6MTcmP");
-            dic.Add("timestamp", DateTime.Now.ToString("yyyyMMddHHmmss"));
-            dic.Add("ext", "1");
-            dic.Add("mobiles", "['13871298164','13618625064']");
-            dic.Add("templateName", "SYS170923110139141");
-            dic.Add("templateParams", "['NO0003319','宝贝','超级珍贵大宝贝']");
-            dic.Add("type", "1");
             return Send(url, dic, token);
         }
 
